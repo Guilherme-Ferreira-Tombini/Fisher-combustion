2949b1f036ccede578884257fd69bd9d9861ad08
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-01-30 17:58:08 -0300

    feat: modified page source

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Jomhuria } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const jomhuria = Jomhuria({
+  subsets: ["latin"],
+  weight: "400"
+});
 
 export const metadata: Metadata = {
   title: "FisherCombustion",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={jomhuria.className}>{children}</body>
     </html>
   );
 }
