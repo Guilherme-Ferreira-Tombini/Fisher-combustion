09a87e7cee9e6a6ef9a0d92fb6738c1e8011f5e5
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-01-30 18:39:28 -0300

    style: modified logo font size and spacing

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="bg-[#8E4837] w-[100%] p-3 text-white">
-     <h1 className="text-lg">
+    <div className="bg-[#8E4837] w-[100%] p-4 text-white">
+     <h1 className="text-5xl">
         FisherCombustion
      </h1>
      
