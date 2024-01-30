15f6cdbc451f069d03495d383a467e572ca6c6e4
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-01-30 17:44:04 -0300

    feat: initialized page header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div className="bg-[#8E4837] w-[100%] p-3 text-white">
+     <h1 className="text-lg">
+        FisherCombustion
+     </h1>
+     
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <div className="bg-[#683529] w-[100%]">
-
+      <Header/>
     </div>
   );
 }
