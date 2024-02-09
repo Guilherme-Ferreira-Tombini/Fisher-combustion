665d7c4423ac825c584442dd0f8090779f5c8fff
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-02-09 15:18:19 -0300

    style: tidy style and position of explanatory topics

--- src/app/components/ExplanatoryTopics.tsx
+++ src/app/components/ExplanatoryTopics.tsx
@@ -2,13 +2,13 @@ import Topics from "./Topics";
 
 export default function ExplanatoryTopics(){
     return(
-        <div className="w-[90%] grid grid-cols-3 mt-3">
+        <div className="w-[90%] grid grid-cols-3 mt-5">
             <Topics 
               image={"/img/cronometro 1.png"} 
-              text={"Os calculos de consumo dos automoveis, muitas vezes, precisam do tempo que o carro percorreu.\n\nMas, no caso do site, só é necessário saber quantos quilometros com 1 litro de combustivel seu veiculo realiza, a quantidade de litros que ele suporta e o tipo de combustivel utilizado."}/>
+              text={"Os calculos de consumo dos automoveis, muitas vezes, precisam do tempo que o carro percorreu.\n\nPorém, no caso do site, só é necessário saber quantos quilometros com 1 litro de combustivel seu veiculo realiza, a quantidade de litros que ele suporta e o tipo de combustivel utilizado."}/>
             <Topics 
               image={"/img/analise-de-dados 1.png"} 
-              text={'Com os resultados dos calculos, pode-se fazer relatórios.\nEsse relatório tem a opção de salvar em PDF, podendo depois consultar no seu proprio dispositivo o arquivo gerado.\nOBS: Os relatórios seram salvos apenas em arquivo PDF não ficaram salvos no site.'}/>
+              text={'Com os resultados dos calculos, pode-se fazer relatórios.\nEsse relatório tem a opção de salvar em PDF, podendo depois consultar no seu proprio dispositivo o arquivo.\n\nOBS: Os relatórios seram salvos apenas em arquivo PDF não ficaram salvos no site.'}/>
             <Topics 
               image={"/img/posto-de-gasolina 1.png"} 
               text={"Neste site calculamos o consumo:\n\n• Quantas vezes será abastecido o automovel para uma viagem;\n• Gasto total para encher o tanque;\n• Gasto para encher meio tanque;\n• Total de quilometros que pode percorrer com o tanque cheio."}/>
--- src/app/components/Topics.tsx
+++ src/app/components/Topics.tsx
@@ -15,14 +15,14 @@ export default function Topics({image, text} : TopicsProps) {
     ));
     return (
         <div className="flex flex-col justify-center p-4 items-center">
-           <div className="bg-[#C35A40] rounded-3xl p-4 w-full h-full grid grid-cols-1 justify-items-center items-center">
+           <div className="bg-[#C35A40] rounded-3xl p-4 w-full h-full grid grid-cols-1 justify-items-center">
               <Image 
                 className="mt-5"
                 src={image} 
                 alt={"Image"} 
                 width={90} height={90}/>
 
-               <h5 className="text-center text-[30px] p-5">
+               <h5 className="text-center text-[30px] m-5">
                   {paragraph}
                </h5>
            </div>
