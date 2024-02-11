b4d91d4db49ca0873c4baa25a39c44dae9df8004
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-02-11 09:27:43 -0300

    style and feat: added part of buttons and placed font-abel

--- src/app/components/Calculate.tsx
+++ src/app/components/Calculate.tsx
@@ -7,15 +7,25 @@ export default function Calculate(){
                 Calculos
             </h1>
             <SymbolForCalculations/>
-            <div className="w-[70%] p-5 mt-3">
+            <div className="w-[70%] p-5 mt-3 font-abel">
                <input 
-                 className="rounded-lg w-full text-center text-3xl placeholder-[#4C4C4C] text-black p-1" 
+                 className="rounded-lg w-full text-center text-2xl placeholder-[#4C4C4C] text-black p-1" 
                  type="text" id="QuilometroPorLitro" 
                  placeholder="Quantos KM seu veiculo anda com 1 litro de combustivel?"/>
                  <input 
-                 className="rounded-lg w-full text-center text-3xl placeholder-[#4C4C4C] text-black p-1 mt-5" 
+                 className="rounded-lg w-full text-center text-2xl placeholder-[#4C4C4C] text-black p-1 mt-5" 
                  type="text" id="TanqueTotal" 
                  placeholder="Quantos litros o tanque de combustivel suporta de combustivel?"/>
+            </div>
+            <div className="w-[250px] flex flex-row justify-between items-center mt-4 font-abel">
+                <input
+                  className="bg-[#8F4330] rounded-lg pr-3 pl-3 pt-3 pb-2  w-[110px] text-2xl" 
+                  type="button" 
+                  value="Calcular" />
+                <input
+                  className="bg-[#920707] rounded-lg pr-3 pl-3 pt-3 pb-2  w-[110px] text-2xl" 
+                  type="button"
+                  value="Limpar" />
             </div>
         </div>
     );
