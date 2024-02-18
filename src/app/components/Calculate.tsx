7119cd4482c60378f36911c3eef822b943a23442
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-02-18 11:56:17 -0300

    style: added hover to the calculation and field clearing buttons

--- src/app/components/Calculate.tsx
+++ src/app/components/Calculate.tsx
@@ -44,11 +44,13 @@ export default function Calculate(){
             <div className="w-[250px] flex flex-row justify-between items-center mt-4 font-abel">
                 <input
                   className="bg-[#8F4330] rounded-lg pr-3 pl-3 pt-3 pb-2  w-[110px] text-2xl" 
+                  id="hoverButton"
                   type="button" 
                   value="Calcular" 
                   onClick={calculate}/>
                 <input
                   className="bg-[#920707] rounded-lg pr-3 pl-3 pt-3 pb-2  w-[110px] text-2xl" 
+                  id="hoverButton"
                   type="button"
                   value="Limpar" 
                   onClick={clear}/>
