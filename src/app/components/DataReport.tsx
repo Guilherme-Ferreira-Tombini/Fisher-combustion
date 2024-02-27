a34030c6c8a7520cf3cfc24fc35c7cf6c89c911e
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-02-27 17:10:16 -0300

    style: modified structures where calculation data will be displayed

--- src/app/components/DataReport.tsx
+++ src/app/components/DataReport.tsx
@@ -2,15 +2,21 @@
 export default function DataReport(props:any){
     return(
         <div className="bg-[#C35A40] rounded-lg p-3 mt-16 w-[83%] grid grid-cols-1 justify-items-center">
-            <h1 className="flex flex-col justify-center items-center leading-none text-[113px]">
+            <h1 className="flex flex-col justify-center items-center leading-none text-[120px] mb-9">
                 RELATÓRIO
             </h1>
-            <div className="w-full grid grid-cols-2">
-                <div className="bg-black h-16">
+            <div className="w-[90%] grid grid-cols-2">
+                <div className="h-16">
+                    <span className=" text-justify text-[45px] text-bold">
+                        Com o tanque cheio o carro percorre:
+                    </span>
+                    <span>
+                        {}
+                    </span>
 
                 </div>
 
-                <div className="bg-white h-16">
+                <div className="h-16 grid grid-cols-1 justify-items-center">
                     
                 </div>
             </div>
