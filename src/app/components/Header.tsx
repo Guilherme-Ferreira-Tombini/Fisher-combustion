8e1189816dbbd12a6dea12665080ca96e26ea746
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-03-07 08:59:03 -0300

    style: modified left margin of header text

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,9 @@
 export default function Header() {
   return (
     <div className="bg-[#8E4837] w-[100%] p-4 ">
-     <h1 className="text-5xl">
-        FisherCombustion
-     </h1>
-     
+      <h1 className="text-5xl ml-5">
+          FisherCombustion
+      </h1>
     </div>
   );
 }
