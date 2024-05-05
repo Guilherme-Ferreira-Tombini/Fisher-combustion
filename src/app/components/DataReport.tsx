760b519c5cfb5d166e9e005eedec1d82b1b48188
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-05-05 15:34:16 -0300

    style:adjusted size and borders of the report part

--- src/app/components/DataReport.tsx
+++ src/app/components/DataReport.tsx
@@ -1,12 +1,12 @@
 "use client"
 export default function DataReport(props:any){
     return(
-        <div className="bg-[#C35A40] rounded-lg p-3 mt-16 w-[83%] grid grid-cols-1 justify-items-center">
-            <h1 className="flex flex-col justify-center items-center leading-none text-[90px] mb-6 mobile:text-[75px]">
+        <div className="bg-[#C35A40] rounded-3xl p-3 mt-16 w-[83%] grid grid-cols-1 justify-items-center">
+            <h1 className="flex flex-col justify-center items-center leading-none text-[89px] mb-6 mobile:text-[75px]">
                 RELATÓRIO
             </h1>
             <div className="w-[90%] grid grid-cols-1 p-2">
-                <div className="flex flex-col justify-items-start text-[20px] font-k2d mobile:text-base">
+                <div className="flex flex-col justify-items-start text-[21px] font-k2d mobile:text-base">
                     <span className="text-justify mt-1">
                         Com o tanque cheio o carro percorre:{" " + props.KMLValue} KM
                     </span>
