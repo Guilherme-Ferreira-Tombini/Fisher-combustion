a04d102cad4dd6ae6dc4707053bc7dc71fb7a625
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-05-09 15:40:38 -0300

    feat: added pdf generation, in adjustments

--- src/app/components/DataReport.tsx
+++ src/app/components/DataReport.tsx
@@ -1,7 +1,26 @@
 "use client"
+import { useEffect } from 'react';
+import jsPDF from 'jspdf';
+
+
 export default function DataReport(props:any){
+    function GerarPDF(){
+              const gerarPDF = () => {
+                const doc = new jsPDF();
+                const content = document.getElementById('content');
+                if(content){
+                    doc.html(content, {
+                    callback: function (pdf) {
+                        pdf.save('Relatorio.pdf');
+                    }
+                    });
+                }
+              };
+              gerarPDF();
+    }
+
     return(
-        <div className="bg-[#C35A40] rounded-3xl p-3 mt-16 w-[83%] grid grid-cols-1 justify-items-center">
+        <div className="bg-[#C35A40] rounded-3xl p-3 mt-16 w-[83%] grid grid-cols-1 justify-items-center" id="content">
             <h1 className="flex flex-col justify-center items-center leading-none text-[89px] mb-6 mobile:text-[75px]">
                 RELATÓRIO
             </h1>
@@ -22,6 +41,14 @@ export default function DataReport(props:any){
                     <span className="mt-3">
                         Seu veiculo está emitindo {props.CO2}kg de CO2 no ambiente.
                     </span>
+                </div>
+                <div className="w-[100%] grid grid-cols-1 justify-items-center font-abel mt-9">
+                    <input 
+                        className="bg-[#8F4330] text-white p-3 rounded-xl text-lg"
+                        id="hoverButton"
+                        type="button" 
+                        value="Gerar PDF" 
+                        onClick={GerarPDF}/>
                 </div>
             </div>
 
