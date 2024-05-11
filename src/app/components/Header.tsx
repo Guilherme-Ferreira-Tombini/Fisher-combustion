1a47ebc7d6a1d4d9d7eb236ba0c702e433c1424c
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-05-11 14:57:00 -0300

    style: added button to the header, in future adjustments

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,12 @@
+import ChangeColor from "./ChangeColor";
+
 export default function Header() {
   return (
-    <div className="bg-[#8E4837] w-[100%] p-4" id="container">
+    <div className="bg-[#8E4837] w-[100%] p-4 flex flex-row justify-between" id="container">
       <h1 className="text-4xl ml-5 ">
           FisherCombustion
       </h1>
+      <ChangeColor/>
     </div>
   );
 }
