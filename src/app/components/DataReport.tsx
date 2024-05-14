16a73c574ad8d42b4c503446c967f79615e18148
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-05-14 15:01:44 -0300

    feat: modified PDF generation

--- src/app/components/DataReport.tsx
+++ src/app/components/DataReport.tsx
@@ -1,31 +1,31 @@
 "use client"
-import { useEffect } from 'react';
+import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 
 
 export default function DataReport(props:any){
-    function GerarPDF(){
-              const gerarPDF = () => {
-                const doc = new jsPDF();
-                const content = document.getElementById('content');
-                if(content){
-                    doc.html(content, {
-                    callback: function (pdf) {
-                        pdf.save('Relatorio.pdf');
-                    }
-                    });
-                }
-              };
-              gerarPDF();
+    function GerarPDF() {
+        const content = document.getElementById("content");
+        if (content) {
+            html2canvas(content)
+                .then(canvas => {
+                    const imgData = canvas.toDataURL('image/png');
+                    const pdf = new jsPDF();
+                    const imgWidth = pdf.internal.pageSize.getWidth();
+                    const imgHeight = (canvas.height * imgWidth) / canvas.width;
+                    pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+                    pdf.save('Relatorio.pdf');
+                });
+        }
     }
 
     return(
-        <div className="bg-[#C35A40] rounded-3xl p-3 mt-16 w-[83%] grid grid-cols-1 justify-items-center" id="content">
+        <div className="bg-[#C35A40] rounded-3xl p-3 mt-16 w-[83%] grid grid-cols-1 justify-items-center">
             <h1 className="flex flex-col justify-center items-center leading-none text-[89px] mb-6 mobile:text-[75px]">
                 RELATÓRIO
             </h1>
-            <div className="w-[90%] grid grid-cols-1 p-2">
-                <div className="flex flex-col justify-items-start text-justify text-[21px] font-k2d mobile:text-base">
+            <div className="w-[90%] grid grid-cols-1 p-2 ">
+                <div className="flex flex-col justify-items-start text-justify text-[21px] font-k2d mobile:text-base bg-[#C35A40] h-[180px]" id="content">
                     <span className="mt-1">
                         Com o tanque cheio o carro percorre:{" " + props.KMLValue} KM
                     </span>
