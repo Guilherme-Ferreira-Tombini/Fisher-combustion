f54897881d6897ced4a96684fb1403bd9bc9efab
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-05-28 11:30:00 -0300

    style: modified page colors and color change button

--- src/app/components/ChangeColor.tsx
+++ src/app/components/ChangeColor.tsx
@@ -23,13 +23,13 @@ export default function ChangeColor(){
         if (header && body && topics && principal && content && footer && seta && botao) {
             header.style.backgroundColor = checked ? '#613327': '#8E4837';
             body.style.backgroundColor = checked ? '#321A15':'#683529';
-            content.style.backgroundColor = checked ? '#613327':'#C35A40';
+            content.style.backgroundColor = checked ? '#7E4434':'#C35A40';
             footer.style.backgroundColor = checked ? '#613327':'#8E4837';
             seta.style.backgroundColor = checked ? '#2C1712': '#C35A40';
             
             changeColors(Array.from(botao), checked ? '#2C1712' : '#8F4330');
-            changeColors(Array.from(topics), checked ? '#613327' : '#C35A40');
-            changeColors(Array.from(principal), checked ? '#613327' : '#C35A40');
+            changeColors(Array.from(topics), checked ? '#7E4434' : '#C35A40');
+            changeColors(Array.from(principal), checked ? '#7E4434' : '#C35A40');
         }
 
     }, [checked]);
@@ -46,7 +46,7 @@ export default function ChangeColor(){
                 onClick={Mudar}
                 checked={checked}
                 onChange={() => {}}/>
-            <div className="relative w-[73px] h-9 bg-[#CF7C68] peer-focus:outline-none peer-focus:ring-7 peer-focus:ring-[#683529] dark:peer-focus:ring-[#9c5342] rounded-full peer dark:bg-[#683529] peer-checked:after:translate-x-full rtl:peer-checked:after:right-0 peer-checked:after:border-none after:content-[''] after:absolute after:top-0.5 after:start-[6px]  after:bg-[#CF7C68] after:border-gray-300 after:border-none after:rounded-full after:h-[30px] after:w-[30px] after:transition-all dark:border-[#9c5342] peer-checked:bg-[#683529]"></div>
+            <div className="relative w-[73px] h-9 bg-[#CF7C68] peer-focus:outline-none peer-focus:ring-7 peer-focus:ring-[#683529] dark:peer-focus:ring-[#9c5342] rounded-full peer dark:bg-[#683529] peer-checked:after:translate-x-full rtl:peer-checked:after:right-0 peer-checked:after:border-none after:content-[''] after:absolute after:top-0.5 after:start-[6px]  after:bg-[#CF7C68] after:border-gray-300 after:border-none after:rounded-full after:h-[30px] after:w-[30px] after:transition-all dark:border-[#9c5342] peer-checked:bg-[#321A15] peer-checked:after:bg-[#8A4938]"></div>
         </label>
     
     )
