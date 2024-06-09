01f577e16291718e6fc33536c455eeb062df9f38
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-06-09 09:45:02 -0300

    style: modified footer hover

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function Footer(){
     return(
         <div className="w-[100%] p-10 grid grid-cols-1 justify-items-center bg-[#8E4837] mt-[200px]" id="footer">
-            <div className="rounded-full bg-[#C35A40] w-[70px] h-[70px] flex flex-col justify-center items-center hoverButton" id="seta">
+            <div className="rounded-full bg-[#C35A40] w-[70px] h-[70px] flex flex-col justify-center items-center hover:scale-110 transition-all duration-500" id="seta">
                 <a href="#container">
                     <Image 
                         src={"/img/Arrow 1.png"} 
